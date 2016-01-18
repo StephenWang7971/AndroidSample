08c81c290ddb528462e46e98905207345ab4fbe5
Stephen Wang <stephen.wang@we-impact.com>
2016-01-18 19:51:12 +0800

    Add robotium test cases.

--- app/src/androidTest/java/com/keelean/citest/ApplicationTest.java
+++ app/src/androidTest/java/com/keelean/citest/ApplicationTest.java
@@ -1,13 +1,43 @@
 package com.keelean.citest;
 
 import android.app.Application;
+import android.test.ActivityInstrumentationTestCase2;
 import android.test.ApplicationTestCase;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.robotium.solo.Solo;
+
+import org.w3c.dom.Text;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
-public class ApplicationTest extends ApplicationTestCase<Application> {
+public class ApplicationTest extends ActivityInstrumentationTestCase2<MainActivity> {
+
+    private static final String TAG = "xxx";
+    private Solo solo;
+
     public ApplicationTest() {
-        super(Application.class);
+        super(MainActivity.class);
     }
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        solo = new Solo(getInstrumentation(), getActivity());
+    }
+
+    @Override
+    protected  void tearDown()  throws Exception {
+        solo.finishOpenedActivities();
+    }
+
+    public void test_default() throws Exception {
+        TextView text = (TextView) solo.getView(R.id.result);
+
+        assertEquals("7 + 4 = 11", text.getText());
+
+    }
+
 }
